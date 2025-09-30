fcdee35d5c7feb44cc7dc3234a7a6cb9ecbd7198
fenrirg6 <rbhbk2@gmail.com>
2025-09-30 18:42:45 +0300

    implemented sys.argv checks and input to make project into reusable CLI tool

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import sys
 from stats import get_num_words, get_count_characters, sort_count_characters
+
+if len (sys.argv) < 2:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+
+filepath = sys.argv[1] # second argument as a path to the book file
 
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
@@ -8,13 +15,13 @@ def get_book_text(filepath):
 
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at", "books/frankenstein.txt", "...")
+    print("Analyzing book found at", filepath, "...")
     print("----------- Word Count ----------")
-    print(f"Found {get_num_words(get_book_text('books/frankenstein.txt'))} total words")
+    print(f"Found {get_num_words(get_book_text(filepath))} total words")
     print("--------- Character Count -------")
 
-    for key in sort_count_characters(get_count_characters(get_book_text('books/frankenstein.txt'))):
-        print(f"{key}: {get_count_characters(get_book_text('books/frankenstein.txt'))[key]}")
+    for key in sort_count_characters(get_count_characters(get_book_text(filepath))):
+        print(f"{key}: {get_count_characters(get_book_text(filepath))[key]}")
 
 
 main()
