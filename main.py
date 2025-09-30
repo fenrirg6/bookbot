191b34f5d08cc5f4deb3fe2a2f1d1a007102e4b4
fenrirg6 <rbhbk2@gmail.com>
2025-09-30 18:28:54 +0300

    Implemented report generation and sorting of character dictionary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_count_characters
+from stats import get_num_words, get_count_characters, sort_count_characters
 
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
@@ -7,7 +7,14 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at", "books/frankenstein.txt", "...")
+    print("----------- Word Count ----------")
     print(f"Found {get_num_words(get_book_text('books/frankenstein.txt'))} total words")
-    print(get_count_characters(get_book_text('books/frankenstein.txt')))
+    print("--------- Character Count -------")
+
+    for key in sort_count_characters(get_count_characters(get_book_text('books/frankenstein.txt'))):
+        print(f"{key}: {get_count_characters(get_book_text('books/frankenstein.txt'))[key]}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,6 @@ def get_count_characters(file_contents):
                 character_counter_dict[char] = 1
 
     return character_counter_dict
+
+def sort_count_characters(character_dictionary):
+    return dict(sorted(character_dictionary.items(), key=lambda x: x[1], reverse=True))
